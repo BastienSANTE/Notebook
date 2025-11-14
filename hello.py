17df8d175460abbe2a9e1d98be32b1bd0cda134f
Bastien Santé | Bas7ien <b.sante@nouvellesimages.xyz>
2025-11-14 17:14:18 +0100

    Quick test for alternating MD/Plain text view. TODO : Create folder for C++ version

--- hello.py
+++ hello.py
@@ -1,35 +1,60 @@
 import sys
-import random
 from PySide6 import QtCore, QtWidgets, QtGui
 
-class MyWidget(QtWidgets.QWidget):
-    def __init__ (self):
+class MyApp(QtWidgets.QMainWindow):
+    def __init__(self):
         super().__init__()
+        self.window_width, self.window_height = 800, 600
+        self.setMinimumSize(self.window_width, self.window_height)
 
-        self.hello = ["Hallo Welt", "Bonjour", "Hei maailma"]
-
-        self.button = QtWidgets.QPushButton("Click!")
-        self.text = QtWidgets.QLabel("Hello World!", alignment=QtCore.Qt.AlignCenter)
-
-        self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
-
-        self.button.clicked.connect(self.magic)
-
+        self.main_window = QtWidgets.QWidget()
+        
+        self.main_layout = QtWidgets.QVBoxLayout(self.main_window)
+        self.mdlayout = QtWidgets.QStackedLayout()
+        self.setCentralWidget(self.main_window)
 
         
-    @QtCore.Slot()
-    def magic(self):
-        self.text.setText(random.choice(self.hello))
+        self.init_ui()
+        self.init_config_signals()
+        
+    def init_ui(self):
+        self.mdview = QtWidgets.QTextEdit()
+        self.rawview = QtWidgets.QTextEdit("$$I = \int \rho R^{2} dV$$")
+        self.switchbutton = QtWidgets.QPushButton(self.main_window)
+
+        self.mdlayout.insertWidget(0, self.rawview)
+        self.mdlayout.insertWidget(1, self.mdview)
+        self.is_md_view = 0
+
+        self.main_layout.addLayout(self.mdlayout)
+        self.main_layout.addWidget(self.switchbutton)
+
+        
+    def init_config_signals(self):
+        self.rawview.textChanged.connect(self.updatemd)
+        self.switchbutton.clicked.connect(self.switchview)
+        
+
+    def updatemd(self):
+        self.mdview.setMarkdown(self.rawview.toPlainText())
 
 
+    def switchview(self):
+        if self.is_md_view == 0:
+            self.mdlayout.setCurrentIndex(1)
+            self.updatemd()
+            self.is_md_view = 1;
+        elif self.is_md_view == 1:
+            self.mdlayout.setCurrentIndex(0)
+            self.is_md_view = 0;
+        
+            
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    myApp = MyApp()
+    myApp.show()
 
-if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
-
-    widget = MyWidget()
-    widget.resize(500, 300)
-    widget.show()
-
-    sys.exit(app.exec())
+    try:
+        sys.exit(app.exec())
+    except SystemExit:
+        print("Process ended")
